fe1ba8581f7bdc0e5118c5f993b087353a0f79c5
esadh <esadh@ESAD>
2021-11-02 13:59:43 +0300

    Proje dosyası ekle.

---
+++ SortedList_uygulamasi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace SortedList_uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kitap_icerigi = new SortedList();
+            kitap_icerigi.Add(1, "Önsöz");
+            kitap_icerigi.Add(50, "Değişkenler");
+            kitap_icerigi.Add(40, "Operatörler");
+            kitap_icerigi.Add(45, "İlişkisel operatörler");
+
+            Console.WriteLine("İçindekiler");
+            Console.WriteLine(new string('-',33));
+            Console.WriteLine($"{"Konular",-33} {"Sayfalar",8}\n");
+            foreach (DictionaryEntry item in kitap_icerigi)
+            {
+                Console.WriteLine($"{item.Value,-33} {item.Key,8}");
+            }
+        }
+    }
+}
